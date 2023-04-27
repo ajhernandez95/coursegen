599ac1bc709c6fad1702105a604869f72ce602a5
Alexander Hernandez <ajhernandez95@live.com>
2023-04-27 17:52:44 -0500

    Fixed responsiveness

--- src/components/CourseOutline/CourseSections.tsx
+++ src/components/CourseOutline/CourseSections.tsx
@@ -6,6 +6,7 @@ import {
   CardHeader,
   Heading,
   SimpleGrid,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { CourseSection as Section } from "./types";
 
@@ -14,11 +15,13 @@ interface CourseSectionProps {
 }
 
 const CourseSections = ({ sections }: CourseSectionProps) => {
+  const templateColumns = useBreakpointValue({
+    base: "repeat(auto-fill, minmax(200px, 1fr))",
+    sm: "repeat(auto-fill, minmax(300px, 1fr))",
+  });
+
   return Number(sections?.length) > 0 ? (
-    <SimpleGrid
-      spacing={4}
-      templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
-    >
+    <SimpleGrid spacing={4} templateColumns={templateColumns}>
       {sections?.map(({ id, title, dates, description }, i) => {
         return (
           <Card variant="outline" key={i}>
