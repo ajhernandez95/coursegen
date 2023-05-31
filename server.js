d17f81e51cfbc0d40c620da3fac054a35060fb61
Alexander Hernandez <ajhernandez95@live.com>
2023-05-30 20:50:58 -0500

    http -> https

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const serveStatic = require("serve-static");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+});
+
 app.use("/", serveStatic(path.join(__dirname, "/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
