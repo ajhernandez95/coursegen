5bdc773b206068836ab3336c9a56f86967888b34
Alexander Hernandez <ajhernandez95@live.com>
2023-06-26 22:35:17 -0500

    Added handles and email

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -3,7 +3,7 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 const AboutUs = () => {
   return (
     <Box p={["1rem 2rem", "1rem 5rem", "1rem 10rem"]}>
-      <Heading>AboutUs</Heading>
+      <Heading>About Us</Heading>
       <Text>
         Welcome to CourseGen, the future of personalized learning. We're an
         innovative AI-driven platform committed to revolutionizing the learning
@@ -21,6 +21,17 @@ const AboutUs = () => {
         we're crafting a unique adventure, designed to cater to your personal
         educational journey. We invite you to join us and discover the joy of
         personalized learning, tailored for you, by you.
+      </Text>
+      <Heading mt="20px">Connect With Us</Heading>
+      <Text>
+        Alexander Hernandez -{" "}
+        <a href="https://twitter.com/pseudoaluxander">@pseudoaluxander</a> |
+        alex@coursegen.ai
+      </Text>
+      <Text>
+        Cole Gottdank -{" "}
+        <a href="https://twitter.com/coleywoleyyy">@coleywoleyyy</a> |
+        cole@coursegen.ai
       </Text>
     </Box>
   );
