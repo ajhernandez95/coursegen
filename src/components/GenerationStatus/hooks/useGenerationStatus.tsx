96374791fe590fb72ea7a628e4b71374383d2525
Alexander Hernandez <ajhernandez95@live.com>
2023-06-28 17:22:59 -0500

    Updated tags

--- src/components/GenerationStatus/hooks/useGenerationStatus.tsx
+++ src/components/GenerationStatus/hooks/useGenerationStatus.tsx
@@ -1,4 +1,11 @@
-import { Stack, Tag, Spinner, ToastPosition, Badge } from "@chakra-ui/react";
+import {
+  Stack,
+  Tag,
+  Spinner,
+  ToastPosition,
+  Badge,
+  Text,
+} from "@chakra-ui/react";
 import endpoints from "../../../services/endpoints";
 import { getReq } from "../../../services/httpClient";
 
@@ -59,16 +66,24 @@ const useGenerationStatus = () => {
           colorScheme={getToastVariant(item)}
           cursor="pointer"
           onClick={() => handleToastClick(item)}
+          maxH="20px"
         >
-          {item.reference_type.charAt(0).toUpperCase() +
-            item.reference_type.slice(1)}{" "}
-          - {item.reference_name}{" "}
-          <Badge colorScheme={getToastVariant(item)} marginLeft="10px">
-            {getFormattedStatus(item.generation_status)}
-          </Badge>
-          {item.generation_status === "in_progress" && (
-            <Spinner marginLeft="5px" size="xs" />
-          )}
+          <Text
+            maxH="20px"
+            overflowWrap="normal"
+            textOverflow="ellipsis"
+            overflowY="hidden"
+          >
+            {item.reference_type.charAt(0).toUpperCase() +
+              item.reference_type.slice(1)}{" "}
+            - {item.reference_name}{" "}
+            <Badge colorScheme={getToastVariant(item)} marginLeft="10px">
+              {getFormattedStatus(item.generation_status)}
+            </Badge>
+            {item.generation_status === "in_progress" && (
+              <Spinner marginLeft="5px" size="xs" />
+            )}
+          </Text>
         </Tag>
       ))}
     </Stack>
